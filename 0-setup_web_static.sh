4d33389f0313531a4830c9492bea27892ae7e2b8
Andrea66bit <njabuloandrea@gmail.com>
2023-11-22 15:57:15 +0000

    static.py

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,62 +1,38 @@
 #!/usr/bin/env bash
-# Installs, configures, and starts the web server
-SERVER_CONFIG="server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+# setup web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
+sudo echo "<html>
+     <head>
+     </head>
+     <body>
+	Holberton School
+     </body>
+     </html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
-	server_name _;
-	index index.html index.htm;
-	error_page 404 /404.html;
-	add_header X-Served-By \$hostname;
+printf %s "server {
+       listen 80 default_server;
+       listen [::]:80 default_server;
+       add_header X-Served-By $HOSTNAME;
+       root /var/www/html;
+       index index.html index.htm;
 
-	location / {
-		root /var/www/html/;
-		try_files \$uri \$uri/ =404;
-	}
+       location /hbnb_static {
+       		alias /data/web_static/current;
+		index index.html index.htm;
+       }
 
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-		try_files \$uri \$uri/ =404;
-	}
-
-	if (\$request_filename ~ redirect_me) {
-		rewrite ^ https://sketchfab.com/bluepeno/models permanent;
-	}
-
-	location = /404.html {
-		root /var/www/error/;
+       location /redirect_me {
+       		return 301 http://nba.com/;
+       }
+       error_page 404 /404.html;
+       location /404 {
+       		root /var/www/html;
 		internal;
-	}
-}"
-HOME_PAGE="<!DOCTYPE html>
-<html lang='en-US'>
-	<head>
-		<title>Home - AirBnB Clone</title>
-	</head>
-	<body>
-		<h1>Welcome to AirBnB!</h1>
-	<body>
-</html>
-"
-# shellcheck disable=SC2230
-if [[ "$(which nginx | grep -c nginx)" == '0' ]]; then
-    apt-get update
-    apt-get -y install nginx
-fi
-mkdir -p /var/www/html /var/www/error
-chmod -R 755 /var/www
-echo 'Hello World!' > /var/www/html/index.html
-echo -e "Ceci n\x27est pas une page" > /var/www/error/404.html
-
-mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo -e "$HOME_PAGE" > /data/web_static/releases/test/index.html
-[ -d /data/web_static/current ] && rm -rf /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data
-bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
-ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
-if [ "$(pgrep -c nginx)" -le 0 ]; then
-	service nginx start
-else
-	service nginx restart
-fi
+       }
+}" > /etc/nginx/sites-available/default
+service nginx restart
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
+# Fabric script that generates a .tgz archive of web_static content
+
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import *
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+    """
+    Return archive path if correctly generated,
+    otherwise Return None
+    """
+    local("mkdir -p versions")
+    dt = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static.{}.tgz".format(dt)
+    tgzip = local("tar -cvzf {} web_static".format(file_path))
+    if tgzip.succeeded:
+        return file_path
+    else:
+        return None
