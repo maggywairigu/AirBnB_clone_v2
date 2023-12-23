9aac53ad8a5ef772b9d579cb3f5c407f16483981
Andrea66bit <njabuloandrea@gmail.com>
2023-12-23 11:35:36 +0000

    control code SCRIPT

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,40 +1,12 @@
-#!/usr/bin/python3
-<<<<<<< HEAD
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'
-"""
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-=======
-'''A simple Flask web application.
-'''
-from flask import Flask
-
-
-app = Flask(__name__)
-'''The Flask application instance.'''
-app.url_map.strict_slashes = False
-
+from flask import flask
+app = flask(__name__)
 
 @app.route('/')
-def index():
-    '''The home page.'''
-    return 'Hello HBNB!'
+@app.route('/hello')
+def Helloworld():
+    return "Hello World"
 
+if __name__ == '__main__';
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
->>>>>>> 131f711afeb69783172c782ec160f5aa9bf663e4
