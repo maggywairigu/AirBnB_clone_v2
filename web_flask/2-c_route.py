39f04016674215d693f1e499bfb78d71aaf5aa87
maggywairigu <wairigumaina2016@gmail.com>
2023-12-31 04:47:54 +0000

    2-c_route.py added

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/pyhton3
+"""
+starts a Flask web application:
+listening on 0.0.0.0, port 5000
+Routes:
+/: Hello HBNB 
+/hbnb: HBNBdisplay  
+/c/<text>:  followed by the value of the text variable
+(replace underscore _ symbols with a space )
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    "Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>"""
+    text = text.replace("_", " ")
+    return "c {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
