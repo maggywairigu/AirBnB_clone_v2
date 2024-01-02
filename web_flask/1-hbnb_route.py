a9293febdc21a1a8349ba8363948d83e36720873
maggywairigu <wairigumaina2016@gmail.com>
2024-01-02 00:28:39 -0800

    1-hbnb_route.py pycodestyle validation fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,17 +3,19 @@
 starts a Flask web application:
  listening on 0.0.0.0, port 5000
 Routes:
-/: Hello 
-/hbnb: HBNBdisplay HBNB!display 
+/: Hello
+/hbnb: HBNBdisplay HBNB!display
 """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
