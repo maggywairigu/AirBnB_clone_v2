7359a5fdedf9ce9e611731e79b1659c6b9ae209c
maggywairigu <wairigumaina2016@gmail.com>
2024-01-02 00:34:27 -0800

    3-python_route.py pycodestyle validation fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Routes:
-/: Hello 
-/hbnb: 
+/: Hello
+/hbnb:
 /c/<text>: , followed by the value of the text variable
 (replace underscore _ symbols with a space )
 /python/<text>: , followed by the value of the text variable
@@ -13,21 +13,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Display 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Display 'HBNB'"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
