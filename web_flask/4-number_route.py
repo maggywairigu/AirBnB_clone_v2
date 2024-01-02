6351f65be4409efac50c4695d43a6ee31da291c0
maggywairigu <wairigumaina2016@gmail.com>
2024-01-02 00:37:52 -0800

    4-number_route.py pycodestyle validation fix

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -16,21 +16,25 @@ from flask import abort
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     """Display Hello HBNB!"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """display 'HBNB'"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """display "C", followed by the value of the text variable"""
     text = text.replace("_", " ")
     return "C {}".format(text)
+
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
